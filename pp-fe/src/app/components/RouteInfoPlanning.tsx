7244967c31d81d9794282a2ca50770a1aff3fc27
advait <advait001@e.ntu.edu.sg>
2023-11-01 00:34:29 +0800

    Adjusted Map Styles

--- pp-fe/src/app/components/RouteInfoPlanning.tsx
+++ pp-fe/src/app/components/RouteInfoPlanning.tsx
@@ -1,57 +1,63 @@
-import React from "react";
-import { View, Text, Image, Button, FlatList, TextInput, Pressable, TouchableOpacity } from "react-native";
-import { useState } from "react";
-
-
-
-
+import React, { useState } from "react";
+import { 
+  View, 
+  Text, 
+  TextInput, 
+  TouchableOpacity, 
+  KeyboardAvoidingView, 
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 
 const RouteInfoPlanning: React.FC = () => {
-
-    const PlanPathButton = ({ onPress, title }) => {
-        return (
-          <TouchableOpacity onPress={onPress}>
-            <View className = "bg-[#ccfbf1] rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg text-left h-12 justify-center">
-              <Text className = "text-[#334155] text-center text-lg font-Poppins_Bold">{title}</Text>
-            </View>
-          </TouchableOpacity>
-        );
-      };
-    
-      const handleButtonPress = () => {
-        // Define the action when the button is pressed
-        console.log('Button pressed!');
-      };
-    
-      const [start_Text, onChangeStartText] = useState('');
-      const [end_Text, onChangeEndText] = useState('');
-
-    return(
-
-<View className = "bg-[#2dd4bf] h-64 rounded-t-xl rounded-b-xl flex-col justify-around pl-4 pr-4">
-  
-        <View className = " flex-col">
-          <Text className = " text-sm font-Poppins_Bold">Start</Text>
-          <TextInput className = " bg-white rounded-t-lg rounded-b-lg  text-left font-Poppins_Light" placeholder = "Starting Address" value = {start_Text} onChangeText={onChangeStartText}/>
-        </View>
-        
-        <View className = " flex-col">
-          <Text className = " text-sm font-Poppins_Bold">End</Text>
-          <TextInput  className = " bg-white rounded-t-lg rounded-b-lg text-left font-Poppins_Light" placeholder = "Ending Address" value = {end_Text} onChangeText={onChangeEndText}/>
-        </View>
-  
-        <PlanPathButton onPress={handleButtonPress} title = "PLAN PATH"/>
-          
-  
-      </View>
+  const PlanPathButton = ({ onPress, title }) => {
+    return (
+      <TouchableOpacity onPress={onPress} className="bg-[#ccfbf1] rounded-lg py-3 mt-4">
+        <Text className="text-[#334155] text-center text-lg font-bold">{title}</Text>
+      </TouchableOpacity>
     );
+  };
 
+  const handleButtonPress = () => {
+    console.log("Button pressed!");
+  };
 
+  const [start_Text, onChangeStartText] = useState("");
+  const [end_Text, onChangeEndText] = useState("");
 
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="bg-[#2dd4bf] p-4 rounded-xl my-4 mx-3 shadow-md">
+          <View className="mb-3">
+            <Text className="text-sm font-bold text-[#334155] mb-1">Start</Text>
+            <TextInput
+              className="bg-white rounded-lg p-2 text-[#334155]"
+              placeholder="Starting Address"
+              value={start_Text}
+              onChangeText={onChangeStartText}
+            />
+          </View>
 
+          <View className="mb-3">
+            <Text className="text-sm font-bold text-[#334155] mb-1">End</Text>
+            <TextInput
+              className="bg-white rounded-lg p-2 text-[#334155]"
+              placeholder="Ending Address"
+              value={end_Text}
+              onChangeText={onChangeEndText}
+            />
+          </View>
 
-
-
+          <PlanPathButton onPress={handleButtonPress} title="PLAN PATH" />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default RouteInfoPlanning;
--- pp-fe/src/app/screens/MapScreen.tsx
+++ pp-fe/src/app/screens/MapScreen.tsx
@@ -1,38 +1,20 @@
 import React from "react";
 import { View, Text, Image, Button, FlatList, TextInput } from "react-native";
-import RouteInfoPlanning from '../components/RouteInfoPlanning';
+import RouteInfoPlanning from "../components/RouteInfoPlanning";
 import MapButtons from "../components/MapButtons";
 
-
-
 const MapPage: React.FC = () => {
+  return (
+    <View className=" h-screen ">
+      <View className="left-0 top-0">
+        <MapButtons />
+      </View>
 
-
-    return(
-
-
-
-            <View className = " h-screen ">
-
-                <View className= "left-0 top-0">
-                    <MapButtons />
-                </View>
-
-                <View className = "bottom-32 inset-x-0 absolute" >
-                    <RouteInfoPlanning />
-                </View>
-                
-            </View>
-
-
-    );
-
-
-
-
-
-
-
+      <View className="bottom-48 inset-x-0 absolute">
+        <RouteInfoPlanning />
+      </View>
+    </View>
+  );
 };
 
 export default MapPage;
