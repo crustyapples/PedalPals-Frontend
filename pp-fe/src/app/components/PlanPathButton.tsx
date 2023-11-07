2e7e22d98fb4dc4dc4165b57134b330b94373b12
advait <advait001@e.ntu.edu.sg>
2023-11-07 15:59:16 +0800

    Fix Path Planning State Logic

--- pp-fe/src/app/components/PlanPathButton.tsx
+++ pp-fe/src/app/components/PlanPathButton.tsx
@@ -2,17 +2,10 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
-  TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableWithoutFeedback,
-  Keyboard,
 } from "react-native";
 import { useAuthDetails } from "../contexts/AuthContext";
-import axios, { AxiosRequestConfig } from "axios";
 import { Picker } from "@react-native-picker/picker";
-import { FlatList } from "react-native-gesture-handler";
 
 const BASE_URL = process.env.EXPO_PUBLIC_BACKEND_API_URL;
 
@@ -23,33 +16,19 @@ type PlanPathButtonProps = {
   onStartClick: any;
 };
 
-// type PlanPathButtonProps = {
-//     onPress: any
-//   };
-
-// const PlanPathButton: React.FC<PlanPathButtonProps> = ({startAddr, endAddr}) => {
-
 const PlanPathButton: React.FC<PlanPathButtonProps> = ({
   startAddr,
   endAddr,
   sendDataToParent1,
   onStartClick,
 }) => {
-  const { getToken, getUserId, getEmail } = useAuthDetails();
+  const { getToken } = useAuthDetails();
   const [token, setToken] = useState("");
-  const [dataToSendParent1, setDataToSendParent1] = useState("");
-
-  const sendDataToParent1OnClick = () => {
-    sendDataToParent1(dataToSendParent1);
-  };
 
   const start_place_id = startAddr[1];
   const end_place_id = endAddr[1];
 
-  const [startLocationResponse, setStartLocationResponse] = useState("");
-  const [endLocationResponse, setEndLocationResponse] = useState("");
-
-  const reverseGeoStart = async () => {
+  const reverseGeo = async (place_id: string) => {
     try {
       const response = await fetch(BASE_URL + "/reverse-geocode", {
         method: "POST",
@@ -57,7 +36,7 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
           Authorization: "Bearer " + token,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ place_id: start_place_id }),
+        body: JSON.stringify({ place_id }),
       });
 
       if (!response.ok) {
@@ -65,52 +44,18 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
         return;
       }
 
-      //   console.log("Start Location")
       const data = await response.json();
-      console.log(data);
       const string_data = JSON.stringify(data);
-      console.log(string_data);
       const trimmed_data = string_data.trim();
       console.log("This is data from reverse geo", trimmed_data);
-      setStartLocationResponse(trimmed_data);
 
-      return { isSuccessful: true, data };
+      return { isSuccessful: true, trimmed_data };
     } catch (error) {
       console.error("Network error:", error);
     }
   };
 
-  const reverseGeoEnd = async () => {
-    try {
-      const response = await fetch(BASE_URL + "/reverse-geocode", {
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ place_id: end_place_id }),
-      });
-
-      if (!response.ok) {
-        console.error("Server responded with an error:", response.statusText);
-        return;
-      }
-
-      //   console.log("End Location")
-      const data = await response.json();
-      console.log(data);
-      const string_data = JSON.stringify(data);
-      const trimmed_data = string_data.trim();
-      console.log("This is data from reverse geo", trimmed_data);
-      setEndLocationResponse(trimmed_data);
-
-      return { isSuccessful: true, data };
-    } catch (error) {
-      console.error("Network error:", error);
-    }
-  };
-
-  const getRoute = async () => {
+  const getRoute = async (start: string, end: string) => {
     try {
       const response = await fetch(BASE_URL + "/get-route", {
         method: "POST",
@@ -119,8 +64,8 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          start_address: startLocationResponse.slice(1, -1),
-          end_address: endLocationResponse.slice(1, -1),
+          start_address: start.slice(1, -1),
+          end_address: end.slice(1, -1),
         }),
       });
 
@@ -131,7 +76,7 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
 
       const data = await response.json();
       console.log("this is data from getRoute", data);
-      setDataToSendParent1(data);
+      sendDataToParent1(data)
     } catch (error) {
       console.error("Network error:", error);
     }
@@ -149,17 +94,9 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
 
   const fetchData = async () => {
     try {
-      // Call the first async function
-      const startResult = await reverseGeoStart();
-      // Call the second async function
-      if (startResult.isSuccessful) {
-        const endResult = await reverseGeoEnd();
-
-        if (endResult.isSuccessful && startLocationResponse && endLocationResponse) {
-          await getRoute();
-        }
-      }
-      // Once the first two functions have completed, call the third async function
+      const startResult = await reverseGeo(start_place_id);
+      const endResult = await reverseGeo(end_place_id);
+      getRoute(startResult.trimmed_data, endResult.trimmed_data);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -168,13 +105,6 @@ const PlanPathButton: React.FC<PlanPathButtonProps> = ({
   const handleButtonPress = () => {
     fetchData();
     onStartClick();
-    sendDataToParent1OnClick();
-
-    // reverseGeoStart();
-    // reverseGeoEnd();
-    // getRoute();
-
-    // onPress;
   };
 
   useEffect(() => {}, []);
